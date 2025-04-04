8bd200093722e07e8664f67d9101cd0a52386f80
Liz Ji <lizji1992@gmail.com>
2025-04-03 23:36:18 -0700

    Fixed issue of assigning vertex colors.

--- op_color_assign.py
+++ op_color_assign.py
@@ -1,4 +1,5 @@
 import bpy
+import bmesh
 
 from . import utilities_color
 from . import utilities_ui
@@ -41,19 +42,22 @@ def assign_color(self, context, index):
 		previous_mode = bpy.context.active_object.mode
 
 	for obj in selected_obj:
-		# Select object
-		bpy.ops.object.mode_set(mode='OBJECT')
-		bpy.ops.object.select_all(action='DESELECT')
-		obj.select_set(True)
-		bpy.context.view_layer.objects.active = obj
+		if obj.type != 'MESH':
+			continue
 
-		# Enter Edit mode
-		bpy.ops.object.mode_set(mode='EDIT')
-
-		if previous_mode == 'OBJECT':
-			bpy.ops.mesh.select_all(action='SELECT')
-		
 		if tt_settings().color_assign_mode == 'MATERIALS':
+			# Select object
+			bpy.ops.object.mode_set(mode='OBJECT')
+			bpy.ops.object.select_all(action='DESELECT')
+			obj.select_set(True)
+			bpy.context.view_layer.objects.active = obj
+
+			# Enter Edit mode
+			bpy.ops.object.mode_set(mode='EDIT')
+
+			if previous_mode == 'OBJECT':
+				bpy.ops.mesh.select_all(action='SELECT')
+
 			# Verify material slots
 			for _ in range(index+1):
 				if index >= len(obj.material_slots):
@@ -65,6 +69,13 @@ def assign_color(self, context, index):
 			obj.active_material_index = index
 			bpy.ops.object.material_slot_assign()
 
+			# restore mode
+			bpy.ops.object.mode_set(mode='OBJECT')
+			bpy.ops.object.select_all(action='DESELECT')
+			for obj in selected_obj:
+				obj.select_set(True)
+			bpy.ops.object.mode_set(mode=previous_mode)
+
 		else:  # mode == VERTEXCOLORS
 			color = utilities_color.get_color(index).copy()
 			if prefs().bool_color_id_vertex_color_gamma:
@@ -73,29 +84,27 @@ def assign_color(self, context, index):
 				color[1] = pow(color[1], 1/gamma)
 				color[2] = pow(color[2], 1/gamma)
 
-			# Manage Vertex Color layer
-			context_override = utilities_ui.GetContextView3D()
-			if not context_override:
-				self.report({'ERROR_INVALID_INPUT'}, "This tool requires an available View3D view.")
-				return {'CANCELLED'}
-			if 'TexTools_colorID' not in obj.data.vertex_colors:
-				obj.data.vertex_colors.new(name='TexTools_colorID')
-			obj.data.vertex_colors['TexTools_colorID'].active = True
+			mesh = obj.data
 
-			# Paint
-			bpy.ops.object.mode_set(mode='VERTEX_PAINT')
-			bpy.context.tool_settings.vertex_paint.brush.color = color
-			bpy.context.object.data.use_paint_mask = True
-			with bpy.context.temp_override(**context_override):
-				bpy.ops.paint.vertex_color_set()
-			bpy.context.object.data.use_paint_mask = False
+			bpy.ops.object.mode_set(mode='OBJECT')
+			if not mesh.vertex_colors:
+				mesh.vertex_colors.new(name="Col")
+			color_layer = mesh.vertex_colors.active
 
-	# restore mode
-	bpy.ops.object.mode_set(mode='OBJECT')
-	bpy.ops.object.select_all(action='DESELECT')
-	for obj in selected_obj:
-		obj.select_set(True)
-	bpy.ops.object.mode_set(mode=previous_mode)
+			bpy.ops.object.mode_set(mode='EDIT')
+			bm = bmesh.from_edit_mesh(mesh)
+
+			# Ensure the bm loops have layer access
+			color_layer_bmesh = bm.loops.layers.color.active
+
+			# Assign red color to selected faces
+			for face in bm.faces:
+				if face.select:
+					for loop in face.loops:
+						loop[color_layer_bmesh] = (color[0], color[1], color[2], 1.0)
+
+			# Update the mesh
+			bmesh.update_edit_mesh(mesh)
 
 	# Show Material or Data Tab
 	utilities_color.update_properties_tab()
